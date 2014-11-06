19f8f7f84be546817a1ab4b2e2392aad9c34db53
Dany <dany.dany@live.ru>
2014-11-06 18:35:04 +0300

    Draw lines with svg

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,4 @@ urlpatterns = patterns(
     'app.views',
     url(r'^(?P<city_id>\d+)/edit/?$', 'edit_map'),
     url(r'^(?P<city_id>\d+)/?', 'view_city'),
-    url(r'^.*', 'view_city')
 )
--- app/views.py
+++ app/views.py
@@ -4,9 +4,9 @@ from app.models import City
 
 @render_to('edit_map.html')
 def edit_map(request, city_id):
-    return {'city': City.objects.get(id=city_id) }
+    return {'city': City.objects.get(id=city_id)}
 
 
 @render_to('index.html')
-def view_city(request, city_id=None):
-    return {}
+def view_city(request, city_id):
+    return {'city': City.objects.get(id=city_id)}
