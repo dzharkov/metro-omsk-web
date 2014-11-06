51b331572ffa741f866babacaddb6ae48459cd1e
Dany <dany.dany@live.ru>
2014-11-06 15:24:39 +0300

    Index view added

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'app.views',
-    url(r'^(?P<id>\d+)/?$', 'edit_map'),
+    url(r'^(?P<city_id>\d+)/edit/?$', 'edit_map'),
+    url(r'^(?P<city_id>\d+)/?', 'view_city'),
+    url(r'^.*', 'view_city')
 )
--- app/views.py
+++ app/views.py
@@ -2,5 +2,10 @@ from annoying.decorators import render_to
 
 
 @render_to('edit_map.html')
-def edit_map(request, id):
-    return {'id': id}
+def edit_map(request, city_id):
+    return {'id': city_id}
+
+
+@render_to('index.html')
+def view_city(request, city_id=None):
+    return {}
