b220c1c777d2e54a2b511813515ae0c56452b38c
Dany <dany.dany@live.ru>
2014-11-06 19:22:53 +0300

    Station selection UI

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from annoying.decorators import render_to
-from app.models import City
+from app.models import City, Station
 
 
 @render_to('edit_map.html')
@@ -9,4 +9,7 @@ def edit_map(request, city_id):
 
 @render_to('index.html')
 def view_city(request, city_id):
-    return {'city': City.objects.get(id=city_id)}
+    return {
+        'city': City.objects.get(id=city_id),
+        'stations': Station.objects.filter(line__city=city_id)
+    }
